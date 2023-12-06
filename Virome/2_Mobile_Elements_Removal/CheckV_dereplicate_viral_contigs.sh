f3a6ecfd7d45053bceb0ddcc4f79219ac118cb6f
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-12-06 10:52:40 +0100

    Update CheckV_dereplicate_viral_contigs.sh

--- Virome/2_Mobile_Elements_Removal/CheckV_dereplicate_viral_contigs.sh
+++ Virome/2_Mobile_Elements_Removal/CheckV_dereplicate_viral_contigs.sh
@@ -22,33 +22,33 @@ module purge; ml CheckV BLAST+; module list
 
 #First, create a blast+ database:
 makeblastdb \
-	-in $contig_file \
-	-dbtype nucl \
-	-out ${contig_file_name}_db.fa
+    -in $contig_file \
+    -dbtype nucl \
+    -out ${contig_file_name}_db.fa
 
 #Next, use megablast from blast+ package to perform all-vs-all blastn of sequences:
 blastn \
-	-query $contig_file \
-	-db ${contig_file_name}_db.fa \
-	-outfmt '6 std qlen slen' \
-	-max_target_seqs 10000 \
-	-out ${contig_file_name}_blast.tsv \
-	-num_threads ${SLURM_CPUS_PER_TASK}
+    -query $contig_file \
+    -db ${contig_file_name}_db.fa \
+    -outfmt '6 std qlen slen' \
+    -max_target_seqs 10000 \
+    -out ${contig_file_name}_blast.tsv \
+    -num_threads ${SLURM_CPUS_PER_TASK}
 
 echo -e '\n---- RUNNING anicalc.py and aniclust.py scripts ----'
 
 #Next, calculate pairwise ANI by combining local alignments between sequence pairs:
 /scratch/p304845/CS_Baby_Biome/2_ME_REMOVAL/anicalc.py \
-	-i ${contig_file_name}_blast.tsv  \
-	-o ${contig_file_name}_ani.tsv
+    -i ${contig_file_name}_blast.tsv  \
+    -o ${contig_file_name}_ani.tsv
 
 #Finally, perform UCLUST-like clustering using the MIUVIG recommended-parameters (95% ANI + 85% AF):
 /scratch/p304845/CS_Baby_Biome/2_ME_REMOVAL/aniclust.py \
-	--fna $contig_file \
-	--ani ${contig_file_name}_ani.tsv \
-	--out ${contig_file_name}_clusters.tsv \
-	--min_ani 95 \
-	--min_tcov 85 \
-	--min_qcov 0
+    --fna $contig_file \
+    --ani ${contig_file_name}_ani.tsv \
+    --out ${contig_file_name}_clusters.tsv \
+    --min_ani 95 \
+    --min_tcov 85 \
+    --min_qcov 0
 
 echo -e '\n---- Dereplication DONE ----'
